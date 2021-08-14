af630b09cdf5036dc6e5bfe6a0f9317b2be64002
collinginsburg <chginsburg@email.wm.edu>
2021-08-13 18:11:27 -0700

    set up tasklist container layout and elements. can't quite get the minmax to work for the tasklist area.

--- src/UI/ui-main.js
+++ src/UI/ui-main.js
@@ -9,6 +9,39 @@ let taskListContainer = document.createElement('div');
 taskListContainer.classList.add('taskListContainer');
 
 
+    let taskListContainerOptions = document.createElement('div');
+    taskListContainerOptions.classList.add('taskListContainer__options');
+        let searchBar = document.createElement('div');
+        searchBar.classList.add('searchBar');
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('btn');
+        let moveButton = document.createElement('button');
+        moveButton.classList.add('btn');
+        let sortByDiv = document.createElement('div');
+        sortByDiv.classList.add('taskListSort');
+            let sortBytxt = document.createElement('p');
+            sortBytxt.classList.add('taskListSort__txt');
+            let sortByDropdown = document.createElement('div');
+            sortByDropdown.classList.add('taskListSort__dropdown');
+            sortByDiv.appendChild(sortBytxt);
+            sortByDiv.appendChild(sortByDropdown);
+        taskListContainerOptions.appendChild(searchBar);
+        taskListContainerOptions.appendChild(deleteButton);
+        taskListContainerOptions.appendChild(moveButton);
+        taskListContainerOptions.appendChild(sortByDiv);
+
+    let taskList = document.createElement('ul');
+    taskList.classList.add('taskList');
+
+    let addTask = document.createElement('div');
+    addTask.classList.add('addTask');
+
+
+taskListContainer.appendChild(taskListContainerOptions);
+taskListContainer.appendChild(taskList);
+taskListContainer.appendChild(addTask);
+
+
 // main
 let main = document.createElement('main');
 main.classList.add('mainApp');
--- src/index.js
+++ src/index.js
@@ -25,11 +25,17 @@ content.appendChild(nav);
 // projectModule.listsArray[0].createTask("cut the cheese", "all i can eat is cheese", "9/1/2021", 3);
 // projectModule.listsArray[0].createTask("bake the cake", "cooking is fun", "12/17/2021", 2);
 
-const project = projectModule;
-project.createList("Home");
+
+// on load
+const project = projectModule; // create overall project
+project.createList("Home"); // create default list and 3 list items
 project.listsArray[0].createTask("take out the trash", "its thursday", "8/12/2021", 3);
 project.listsArray[0].createTask("flip the pickles", "they leaking!", "8/15/2021", 2);
 project.listsArray[0].createTask("walk the dog", "or else he'll poop in the house", "Today", 3);
+
+//set regulator module to selected project
+let defaultListGUID = project.listsArray[0].getGUID();
+console.log(defaultListGUID);
 
 console.log(project);
 
